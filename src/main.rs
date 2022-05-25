b78b6dedf76af9f3658c02922f6bb0ad1dda3f37
Nobody <74131447+wdsanoq@users.noreply.github.com>
2022-05-26 02:29:35 +0300

    Move windows

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-#![windows_subsystem = "windows"]
+// #![windows_subsystem = "windows"]
 extern crate user32;
 extern crate winapi;
 
 use clap::{Parser, Subcommand};
-use winapi::shared::windef::POINT;
-use winapi::um::winuser::SW_SHOW;
+use simple_logger::SimpleLogger;
+use std::mem;
+use winapi::shared::windef::{POINT, RECT, SIZE};
+use winapi::um::winuser::{MONITORINFO, MONITOR_DEFAULTTONEAREST, SW_SHOW};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -17,6 +19,8 @@ enum Commands {
     /// Focus topmost window at x y
     #[clap(arg_required_else_help = true)]
     FocusAt { x: i32, y: i32 },
+    #[clap(arg_required_else_help = true)]
+    MoveToMonitor { monitor: i32 },
 }
 
 fn focus_region(x: i32, y: i32) {
@@ -28,10 +32,39 @@ fn focus_region(x: i32, y: i32) {
     }
 }
 
+fn move_to_monitor(monitor: i32) {
+    SimpleLogger::new().init().unwrap();
+    let mut rect: RECT = unsafe { mem::zeroed() };
+    let mut wsize = SIZE { cx: 0, cy: 0 };
+    let mut monitor_info: MONITORINFO = unsafe { mem::zeroed() };
+    monitor_info.cbSize = mem::size_of::<MONITORINFO>().try_into().unwrap();
+
+    unsafe {
+        let active_window_handle = winapi::um::winuser::GetForegroundWindow();
+        winapi::um::winuser::GetWindowRect(active_window_handle, &mut rect);
+        let aminfo =
+            winapi::um::winuser::MonitorFromWindow(active_window_handle, MONITOR_DEFAULTTONEAREST);
+
+        winapi::um::winuser::GetMonitorInfoW(aminfo, &mut monitor_info);
+
+        wsize.cx = rect.right - rect.left;
+        wsize.cy = rect.bottom - rect.top;
+        // winapi::um::winuser::MoveWindow(
+        //     active_window_handle,
+        //     -1800,
+        //     40,
+        //     wsize.cx,
+        //     wsize.cy,
+        //     monitor,
+        // );
+    }
+}
+
 fn main() {
     let args = Cli::parse();
 
     match &args.command {
         Commands::FocusAt { x, y } => focus_region(*x, *y),
+        Commands::MoveToMonitor { monitor } => move_to_monitor(*monitor),
     }
 }
